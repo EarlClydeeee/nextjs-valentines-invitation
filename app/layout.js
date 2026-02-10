a55842763e27eec852fb8ba2b8555ea8035db5b3
Earl Clyde Bañez <earlclyde.mbanez@gmail.com>
2026-02-11 01:10:20 +0800

    fix

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 
 export const metadata = {
-  title: '💕 A Special Invitation',
+  title: 'For You',
   description: 'A romantic Valentine\'s picnic date proposal',
   icons: {
     icon: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">💕</text></svg>',
